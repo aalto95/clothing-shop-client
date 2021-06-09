7edd55da1874a0c106efe9b6c353d5ff16189c5e
y2k01 <stanportableuse@gmail.com>
2021-06-09 15:02:39 +0900

    new footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,8 @@ import styles from './Footer.module.css'
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <h1>Footer</h1>
+            <h1>Grocery Store</h1>
+            <p>Designed and built by <a href="https://github.com/y2k01">Pavlov Stanislav</a></p>
         </footer>
     )
 }
