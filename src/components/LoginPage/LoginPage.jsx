9d6296f52f3a75490bd05d6a8e32492b01450015
y2k01 <stanportableuse@gmail.com>
2021-07-05 11:56:40 +0900

    login page fix

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -13,14 +13,23 @@ const LoginPage = (props) => {
     return (
        <div className={styles.loginComponent}>
            { props.isLogged && <Redirect to='/admin'/> }
-           <NavLink to="/"><h1 className={styles}>Grocery Store</h1></NavLink>
            <div className={styles.formWrapper}>
                <form action="" className={styles.loginForm} onSubmit={onFormSubmit}>
-                   <label htmlFor="username">USERNAME:</label>
-                   <input type="text" name="username" onChange={props.onLoginChange} value={props.login}/>
-                   <label htmlFor="password">PASSWORD:</label>
-                   <input type="text" name="password" onChange={props.onPasswordChange} value={props.password}/>
-                   <input type="submit" className={styles.submitBtn} value="LoginPage" disabled={!props.password && !props.login}/>
+                   <input
+                       type="text"
+                       name="username"
+                       placeholder="username"
+                       onChange={props.onLoginChange}
+                       value={props.login}
+                   />
+                   <input
+                       type="text"
+                       name="password"
+                       placeholder="password"
+                       onChange={props.onPasswordChange}
+                       value={props.password}
+                   />
+                   <input type="submit" className={styles.submitBtn} value="Log in" disabled={!props.password && !props.login}/>
                </form>
            </div>
        </div>
