3d172767170e5ac2a63da75f26b1ddeb140019fb
aaltoo <scotlandfjord@protonmail.com>
2022-11-08 19:50:23 +0900

    Format the rest files

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import {store} from "./app/store";
-import reportWebVitals from './reportWebVitals';
-import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import { store } from "./app/store";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
-      <Provider store={store}>
-          <App />
-      </Provider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
